857125a82078773089bbfe78cb51220c0b4212b0
JoseEdeNeto <jose.ede.neto@gmail.com>
2019-09-05 20:31:23 -0300

    Classificadores

--- machine learning/classificador.py
+++ machine learning/classificador.py
@@ -1,4 +1,7 @@
+import numpy as np
 import random
+import pandas as pd
+import sklearn.preprocessing as preprocessing
 from sklearn.model_selection import cross_validate, StratifiedKFold, GridSearchCV, train_test_split
 from matplotlib import pyplot
 from sklearn.neighbors import KNeighborsClassifier
@@ -12,35 +15,81 @@ class Classificador():
               self._seed = seed
 
        def classificar(self, dataset):
-              X = dataset[:,:-1] # seleciona todos os atributos menos o ultimo
-              y = dataset['target'] # seleciona o "rótulo" que será classificado
-              random.seed(self._seed) # seed
+              X = dataset.loc[:, dataset.columns != 'target'] ## seleciona todos os atributos menos o target
+              y = dataset['target'] ## seleciona o "rótulo" que será classificado
+
+              random.seed(self._seed) ## gera seed
 
               ## StratifiedKfold separa os folds preservando a porcentagem de amostras para cada classe do target ##
               # n_splits = número de folds
               # random_state = seed
               kfold = StratifiedKFold(n_splits = 10, random_state = self._seed)
-              modelo = self.get_classificador(X, y) #? TODO
+              modelo = self.get_classificador(X, y)
 
               ## cv = método de cross validation
               ## scoring = métricas para avaliação do modelo
               ## f1_macro = calcula f1 separado por classes, mas sem estabelecer pesos
-              scores = cross_validate(modelo, X, y, cv = kfold, scoring = ['accuracy', 'f1_macro', 'precision', 'recall'])
+              scores = cross_validate(modelo, X, y, cv = kfold, scoring = ['accuracy', 'f1_macro', 'precision_macro', 'recall_macro'])
               print("Accuracy: %s on average and %s SD" %
                      (scores['test_accuracy'].mean(), scores['test_accuracy'].std()))
               print("Precision: %s on average and %s SD" %
-                     (scores['test_precision'].mean(), scores['test_precision'].std()))
+                     (scores['test_precision_macro'].mean(), scores['test_precision_macro'].std()))
               print("Recall: %s on average and %s SD" %
-                     (scores['test_recall'].mean(), scores['test_recall'].std()))
+                     (scores['test_recall_macro'].mean(), scores['test_recall_macro'].std()))
               print("F-measure: %s on average and %s SD" %
                      (scores['test_f1_macro'].mean(), scores['test_f1_macro'].std()))
 
               X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
               modelo.fit(X_train, y_train)
 
-              dataset['%s_scores' % self.classificador] = scores
               return dataset
 
-class DecisionTreeClassificador(Classificador):
-    def __init__ (self, seed = 42):
-        self._seed = seed
+class DecisionTree(Classificador):
+       def __init__ (self, seed = 42):
+              Classificador.__init__(self, seed)
+              self.tipo = 'decision_tree'
+
+       def get_classificador(self, X, y):
+              print('-- DECISION TREE --')
+              model = DecisionTreeClassifier(random_state=self._seed)
+              return model
+
+class KNN(Classificador):
+       def __init__ (self, seed = 42):
+              Classificador.__init__(self, seed)
+              self.tipo = 'decision_tree'
+
+       def get_classificador(self, X, y):
+              print('-- KNN --')
+              model = KNeighborsClassifier()
+              return model
+
+class SVM(Classificador):
+       def __init__ (self, seed = 42):
+              Classificador.__init__(self, seed)
+              self.tipo = 'decision_tree'
+
+       def get_classificador(self, X, y):
+              print('-- SVM --')
+              model = SVC(random_state=self._seed)
+              return model
+
+class RandomForest(Classificador):
+       def __init__ (self, seed = 42):
+              Classificador.__init__(self, seed)
+              self.tipo = 'decision_tree'
+
+       def get_classificador(self, X, y):
+              print('-- RANDOM FOREST --')
+              model = RandomForestClassifier(random_state=self._seed)
+              return model
+
+class LR(Classificador):
+       def __init__ (self, seed = 42):
+              Classificador.__init__(self, seed)
+              self.tipo = 'decision_tree'
+
+       def get_classificador(self, X, y):
+              print('-- LOGISTIC REGRESSION --')
+              model = LogisticRegression(random_state=self._seed)
+              return model
--- machine learning/inicial.py
+++ machine learning/inicial.py
@@ -4,22 +4,44 @@ import pandas as pd
 import sklearn.preprocessing as preprocessing
 import classificador
 
-path_file1 = 'C:/personalstuff/DataSets/DataSets/training-widget.tab'
+path_file1 = 'DataSets/widgets/training-widget.tab'
 file1 = pd.read_csv(path_file1, sep='\t')
 
-path_file2 = 'C:/personalstuff/DataSets/DataSets/test-widget.tab'
+path_file2 = 'DataSets/widgets/test-widget.tab'
 file2 = pd.read_csv(path_file2, sep='\t')
 
 frames = [file1, file2]
 dataset = pd.concat(frames)
 
-print(dataset.head(40))
-
 labels = ['Dropdown', 'Suggestionbox', 'Other'] # labels do dataset
 labelencoder = preprocessing.LabelEncoder() # codificador do sklearn
 labelencoder = labelencoder.fit(labels) # codifica as labels em valores de 0 até n-1 / 1 - other / 2 
 bc_labelencoded = labelencoder.transform(dataset.widgetClass) # faz a transformação da codificação para os indexes no dataset
+# 0 - dropdown
+# 1 - other
+# 2 - suggestionbox
 
 dataset['target'] = bc_labelencoded # salva na variável target o valor das classes codificadas
+dataset = dataset.drop(columns="widgetClass")
 
-print(dataset['target'].value_counts())
+labels = ['none', 'mouseover', 'keyup', 'click'] # labels do dataset
+labelencoder = preprocessing.LabelEncoder() # codificador do sklearn
+labelencoder = labelencoder.fit(labels) # codifica as labels em valores de 0 até n-1 / 1 - other / 2 
+bc_labelencoded = labelencoder.transform(dataset.event) # faz a transformação da codificação para os indexes no dataset
+
+dataset.event = bc_labelencoded
+
+decisionTree = classificador.DecisionTree()
+decisionTree.classificar(dataset)
+
+knn = classificador.KNN()
+knn.classificar(dataset)
+
+svm = classificador.SVM()
+svm.classificar(dataset)
+
+randomForest = classificador.RandomForest()
+randomForest.classificar(dataset)
+
+logisticRegression = classificador.LR()
+logisticRegression.classificar(dataset)
